f53c54a9ec2e6e264b9a7dfd5e5987fe42207500
rendermani <ml@webrender.de>
2019-01-31 14:14:39 +0100

    Set userId

--- getcontact.php
+++ getcontact.php
@@ -6,8 +6,8 @@ use ascio\whmcs\ssl as ssl;
 use ascio\whmcs\ssl\SslContacts;
 use ascio\whmcs\ssl\Params;
 header('Content-Type: application/json');
-
 $params = new Params();
+$params->userId = $_SESSION["uid"];
 $contacts = new SslContacts($params);
 echo json_encode($contacts->getFromApi($_GET["contactId"],false));
 
