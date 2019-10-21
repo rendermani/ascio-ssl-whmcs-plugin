deaad4b6d8eb181749d744a619eeee6b29b92fda
rendermani <ml@webrender.de>
2019-10-21 21:41:02 +0200

    refactor

--- approval-addresses.php
+++ approval-addresses.php
@@ -6,8 +6,9 @@ header('Content-Type: application/json');
 
 use ascio\whmcs\ssl\Fqdn;
 use ascio\whmcs\ssl\Ssl; 
+use ascio\whmcs\ssl\Params; 
 
 $fqdn = new Fqdn($_POST["fqdn"]);
-$ssl = new Ssl([]);
+$ssl = new Ssl(new Params());
 echo json_encode(["html"=>$ssl->getApprovalAddresses($fqdn)]);
 
--- lib/Error.php
+++ lib/Error.php
@@ -4,7 +4,7 @@ namespace ascio\whmcs\ssl;
 class AscioException extends \Exception {
     private $templateCode;
     private $result;
-    public function __construct ($message,$code,$templateCode=null,$result=null) {
+    public function __construct ($message,$code=500,$templateCode=null,$result=null) {
         $this->result = $result;
         $this->templateCode = $templateCode;
         parent::__construct($message, $code, null);
@@ -13,16 +13,16 @@ class AscioException extends \Exception {
         return $this->templateCode;
     }
     public function getResult() {
-        return $result;
+        return $this->result;
     }
 }
 class AscioUserException extends AscioException {
-    public function __construct ($message,$code,$templateCode=null,$result=null) {
+    public function __construct ($message,$code=500,$templateCode=null,$result=null) {
         parent::__construct($message, $code, $templateCode,$result);
     }
 }
 class AscioSystemException extends AscioException {
     public function __construct ($message,$code=500,$result=null) {
-        parent::__construct($message, $code, $templateCode,$result);
+        parent::__construct($message, $code, null,$result);
     }
 }
